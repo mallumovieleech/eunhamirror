025bf60180d7c7aef502a91631a22decb846373f
mallumovieleech <88672268+mallumovieleech@users.noreply.github.com>
2022-09-29 12:23:59 +0530

    Update __main__.py
    Signed-off-by: mallumovieleech <88672268+mallumovieleech@users.noreply.github.com>

--- bot/__main__.py
+++ bot/__main__.py
@@ -46,8 +46,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Channel", "https://t.me/Eunha_Mirror")
-    buttons.buildbutton("Group", "https://t.me/Eunha_Mirror")
+    buttons.buildbutton("Owner", "https://t.me/zkp143")
+    buttons.buildbutton("Group", "https://t.me/MalluMoviesGroup3")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
